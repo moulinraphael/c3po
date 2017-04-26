b2250eeadebf99c87d422f0dee7ff58d2f1cd5e8
Raphaël Moulin <raphaelm@theodo.fr>
2017-04-26 17:47:25 +0200

    Create simple user page

--- routes/admin.js
+++ routes/admin.js
@@ -5,4 +5,8 @@ router.get('/', function(req, res, next) {
   	res.render('admin', {users: req.app.get('users')});
 });
 
+router.get('/:user_id', function(req, res, next) {
+  	res.render('user', {user_id: req.params.user_id});
+});
+
 module.exports = router;
