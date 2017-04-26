6416c167a473ce56c88ad8e3d5033fa225f5e2f9
Raphaël Moulin <raphaelm@theodo.fr>
2017-04-26 17:54:58 +0200

    ajout erreur users inconnu

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,13 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:user_id', function(req, res, next) {
-  	res.render('user', {user_id: req.params.user_id});
+	var user_id = req.params.user_id;
+	var users = req.app.get('users');
+    if (!users[user_id]) {
+    	res.sendStatus(404);
+    } else {
+  		res.render('user', {user_id: user_id});
+    } 
 });
 
 module.exports = router;
